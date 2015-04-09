6c162ed555e8ed5373231ba6f98a638c136389e0
Wylis <cruxx@gmx.net>
2015-04-09 22:33:59 +0200

    some experiments

--- src/main/java/org/wylis/gates/Gates.java
+++ src/main/java/org/wylis/gates/Gates.java
@@ -1,30 +1,29 @@
 package org.wylis.gates;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.registry.LanguageRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import net.minecraft.init.Items;
-import net.minecraft.item.ItemStack;
+import org.wylis.gates.blocks.DoubleDoors;
 
 @Mod(modid = Gates.MODID, name = Gates.NAME, version = Gates.VERSION)
 public class Gates
 {
-	public static final String MODID = "gates";
+    public static final Block doubleDoors = new DoubleDoors(Material.wood).setHardness(3.0F).setStepSound(Block.soundTypeWood).setCreativeTab(CreativeTabs.tabBlock).setBlockName("doorDoubleDoors").setBlockTextureName("door_wood");
+
+    public static final String MODID = "gates";
     public static final String NAME = "Wylis Gates";
     public static final String VERSION = "0.0.1";
 
     @Mod.Instance("Gates")
     public static Gates instance;
-    
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        GameRegistry.registerBlock(doubleDoors, "DoubleDoors");
     }
 }
---
+++ src/main/java/org/wylis/gates/blocks/DoubleDoors.java
@@ -0,0 +1,10 @@
+package org.wylis.gates.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class DoubleDoors extends Block {
+    public DoubleDoors(Material material) {
+        super(material);
+    }
+}
